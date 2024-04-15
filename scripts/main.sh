a2c185a592da554c42256aac625031da93129cca
maanisim <michal.anisimow@gmail.com>
2024-04-15 16:26:58 +0100

    Update main.sh

--- scripts/main.sh
+++ scripts/main.sh
@@ -1,4 +1,9 @@
+# bat - syntax highlight
+# https://github.com/sharkdp/bat
 wget -O bat.zip https://github.com/sharkdp/bat/releases/download/v0.24.0/bat-v0.24.0-x86_64-unknown-linux-musl.tar.gz
 tar -xvzf bat.zip -C /usr/local
 mv /usr/local/bat-v0.24.0-x86_64-unknown-linux-musl /usr/local/bat
 alias bat='/usr/local/bat/bat'
+# manf - man flag
+# Example usage "manf grep -s"
+function manf () { man $1 | less -p "^ +$2"; }
