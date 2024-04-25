6413805b941ff2334333247f9e16edd8352e50ea
maanisim <michal.anisimow@gmail.com>
2024-04-25 14:22:14 +0100

    added function to find man flag

--- scripts/eb.sh
+++ scripts/eb.sh
@@ -1,3 +1,6 @@
 sudo yum install python-pygments -y
 alias ccat="pygmentize -g"
 sudo yum install htop -y
+pip install thefuck
+eval $(thefuck --alias)
+function manf () { man $1 | less -p "^ +$2"; }
